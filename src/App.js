2dfc4106b6265166c562f9d563235858f2674be2
Elijah Meshnick <emeshnick@gmail.com>
2021-02-01 17:29:39 -0500

    chore: input adds to database

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Input from "./components/Input";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>Hello World</h1>
+        <Input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
