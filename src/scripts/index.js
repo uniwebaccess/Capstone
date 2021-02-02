2dfb55567628bda8ae6d8f7ff6994f44ac6170a3
Khrystyna Tsiupa <vanderwooodsen@gmail.com>
2021-02-02 13:11:01 -0800

    Created backend.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,53 @@
-const puppeteer = require ('puppeteer');
-const checkHeders = require ('./checkHtag');
-const checkImgAlt = require ('./checkImg');
+const puppeteer =  require('puppeteer');
+
+
+async function checkImgAlt(page) {
+  let result = await page.$$eval("img", imgs => {
+    let count = imgs.length;
+
+    let withAlt = 0;
+    for (let img of imgs) {
+      if(img.getAttribute("alt")) {
+        withAlt++;
+
+      }
+    }
+    return {
+      count: count,
+      withAlt: withAlt
+    }
+  })
+  let percent = Math.floor(result.withAlt*100/result.count);
+  return {
+    allImages: result.count,
+    imagesWithAlt: result.withAlt,
+    percent: percent,
+    passed: percent > 60
+  }
+}
+
+async function checkHeders(page){
+  let headers = await page.$$eval('h1, h2, h3, h4, h5, h6', (headers)=>{
+    return headers.map(header => header.tagName)
+  })
+
+  let numbers = headers.map(header => header[1]*1);
+
+  let wrongHeaders = 0;
+  let currentLevel = 0;
+  for (let level of numbers) {
+    if (level > currentLevel + 1) {
+      wrongHeaders++;
+    }
+
+    currentLevel = level;
+  }
+
+  return {
+    wrongHeaders: wrongHeaders,
+    passed: wrongHeaders == 0
+  }
+};
 
 async function checkPage(url) {
   const browser = await puppeteer.launch();
@@ -21,9 +68,7 @@ async function checkPage(url) {
 
 async function main(url) {
   let score = await checkPage(url);
-  return score;
+  console.log(score);
 }
 
-export default main;
-
-//"https://en.wikipedia.org/wiki/Penguin"
+ export default main();
