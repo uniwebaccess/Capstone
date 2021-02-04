c22830b8413204dc759e330295272a339fbd273b
Elijah Meshnick <emeshnick@gmail.com>
2021-02-04 15:42:07 -0500

    chore: firebase moved to back end

--- backend/api.js
+++ backend/api.js
@@ -1,25 +1,45 @@
-// import database from "../src/components/Firebase/firebase"
-// const database = require("../src/components/Firebase/firebase")
-const router = require('express').Router();
-const checker = require('./checker')
+const database = require("../src/components/Firebase/firebase");
+const router = require("express").Router();
+const checker = require("./checker");
+
 //ROUTE /API/
-router.post('/url', async (req, res, next) => {
+router.get("/test/:urlKey", async (req, res, next) => {
+  console.log("outside of try catch in get route");
   try {
-    console.log("req.body.url", req.body.url)
-    const result = await checker(req.body.url)
-    // console.log("result", result)
+    console.log("inside of try catch in get route");
+    const ref = database.ref("/scans/" + req.params.urlKey);
+    console.log("req.body.urlKey", req.params.urlKey);
 
-    // await database
-    //   .ref("/scans/" + req.body.urlKey)
-    //   .set({ url: req.body.url, data: result })
-
-    res.json(result)
+    // .once retrieves data once wheras .on would continuously update
+    ref.once("value", (snapshot) => {
+      if (snapshot.exists()) {
+        const retrievedData = snapshot.val();
+        res.json(retrievedData);
+      } else {
+        console.log("snapshot not found");
+        res.json({ url: "", data: null });
+        // res.sendStatus(500);
+      }
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.post("/test", async (req, res, next) => {
+  try {
+    const result = await checker(req.body.url);
+
+    await database
+      .ref("/scans/" + req.body.urlKey)
+      .set({ url: req.body.url, data: result });
+
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
 
 // "start": "react-scripts start"
-
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,12 +1,9 @@
-const firebase = require('firebase/app')
+const firebase = require("firebase/app");
+require("firebase/database");
 // require("firebase/database")
-
-
 
 // import firebase from "firebase/app";
 // import "firebase/database";
-
-
 
 const config = {
   apiKey: "AIzaSyAfpyLcY4yCvumsOOieL8pBrI6xLJ5mGaQ",
@@ -19,9 +16,9 @@ const config = {
   measurementId: "G-C63Y08YQ2K",
 };
 
-firebase.initializeApp(config);
+firebase.default.initializeApp(config);
 
-const database = firebase.database();
+const database = firebase.default.database();
 
 // export default database;
-module.exports = database
+module.exports = database;
