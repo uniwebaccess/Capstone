dca78221ce5d860542d0f09303480eb46558b269
KseniaRo <kseniaro@yahoo.com>
2021-02-05 12:13:31 -0800

    checked test

--- backend/checker/headings.js
+++ backend/checker/headings.js
@@ -5,18 +5,6 @@ async function headingsCheck(page) {
     return headers.map(header => header.tagName)
   })
 
-  const logicSequenceFunc = (headers) => {
-    let check = []
-    for (let i = 0; i < headers.length; i++) {
-      if (!check.includes(headers[i])) {
-        check.push(headers[i])
-      }
-    }
-    for (let i = 0; i < check.length - 1; i++) {
-      if (check[i] > check[i + 1]) return false
-      return true
-    }
-  }
 
   let numbers = headers.map(header => header[1] * 1);
 
@@ -28,7 +16,7 @@ async function headingsCheck(page) {
     }
     currentLevel = level;
   }
-  const skipHeaderFunc = () => {
+  const logicSequenceFunc = () => {
     if (wrongHeaders !== 0) return false
     return true
   }
@@ -41,12 +29,12 @@ async function headingsCheck(page) {
     return false
   }
 
-  const checkHeaderTag = await page.$('header')
-  const checkSectionTag = await page.$('section')
+  // const checkHeaderTag = await page.$('header')
+  // const checkSectionTag = await page.$('section')
 
   const h1OnlyOne = h1()
-  const skipHeader = skipHeaderFunc()
-  const logicSequence = logicSequenceFunc(headers)
+  // const skipHeader = skipHeaderFunc()
+  const logicSequence = logicSequenceFunc()
   //order is important!! and based on testNames(should match)
   let calculateEachTest = []
 
@@ -60,15 +48,9 @@ async function headingsCheck(page) {
     } else {
       calculateEachTest.push(100)
     }
-    if (skipHeader === false) {
-      total -= 25
-      calculateEachTest.push(10)
-    } else {
-      calculateEachTest.push(100)
-    }
 
     if (logicSequence === false) {
-      total -= 25
+      total -= 30
       calculateEachTest.push(10)
     } else {
       calculateEachTest.push(100)
@@ -94,12 +76,13 @@ async function headingsCheck(page) {
   calculateEachTest.push(totalPercent)
   return {
     onlyOneH1: h1OnlyOne,
-    skipHeader: skipHeader,
     logicSequence: logicSequence,
     total: totalPercent,
     // checkHeaderTag: checkHeaderTag,
     // checkSectionTag: checkSectionTag,
-    testNames: ["H1 Tag", "Correct Order of H-Tags", "Logic Sequence",
+    testNames: ["H1 Tag",
+      // "Correct Order of H-Tags",
+      "Logic Sequence",
       // "Header Tag", "Section Tag",
       "total"],
     eachTest: calculateEachTest,
