58da0503a9d6fb4c6eddb9933c4bce72caf1ec8a
Elijah Meshnick <emeshnick@gmail.com>
2021-02-10 17:37:54 -0500

    fix: commented out bug code

--- client/src/components/single-tests/TestFieldDescription.js
+++ client/src/components/single-tests/TestFieldDescription.js
@@ -16,15 +16,20 @@ const navStyles = (theme) => ({
   },
   description: {
     color: "#616161",
-    fontSize: "20px",
+    fontSize: "15px",
     margin: "40px",
     marginTop: "10px",
+    marginBottom: "10px",
+  },
+  passing: {
+    fontSize: "8px",
+    fontWeight: "bold",
     marginBottom: "20px",
   },
 });
 class TestFieldDescription extends Component {
   render() {
-    const { selectedField, descriptions, classes } = this.props;
+    const { selectedField, avgData, descriptions, classes } = this.props;
     return (
       <Container className={classes.root} align="center">
         <Box
@@ -43,6 +48,12 @@ class TestFieldDescription extends Component {
           <Typography className={classes.description}>
             {descriptions[selectedField]}
           </Typography>
+          {selectedField === "images" && avgData["images"] && (
+            <Typography className={classes.passing} align="center">
+              Out of {avgData["total-scans"]} scans,{" "}
+              {avgData[selectedField]["passed"]} have passed this test.
+            </Typography>
+          )}
         </Box>
       </Container>
     );
@@ -52,6 +63,7 @@ class TestFieldDescription extends Component {
 const mapState = (state) => {
   return {
     selectedField: state.data.selectedField,
+    avgData: state.data.avgData,
   };
 };
 
