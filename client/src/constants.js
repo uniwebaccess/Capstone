9fcb46ea583ea305ac819790684a7dc643a750b3
Beth Howell <bethhowell123@gmail.com>
2021-02-10 11:47:50 -0500

    update descriptions in constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,10 @@
 export const checkerDescriptions = {
-  globalCode: "This is the globalCode description",
-  headings: "This is the headings description",
-  images: "This is the images description",
+  globalCode:
+    'Global code is code that affects your entire website or web app.',
+  headings:
+    'Heading elements (such as h1, h2, h3, etc.) help break up the content of the page into related sets of information. They are important for helping people who use assistive technology like screen-readers to understand the meaning of a page and its content.',
+  images:
+    "Images are a common way to convey a variety of information on websites.  Having accessible code that uses 'alt' attributes ensures that screen readers are able to describe your images to viewers with differing levels of visual ability",
+  controls:
+    'Controls are interactive elements such as links and buttons that let a person navigate to a destination or perform an action on your website or app.',
 };
