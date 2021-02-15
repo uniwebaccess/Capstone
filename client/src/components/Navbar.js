544261f311ead76bd6ccb263be73e9b0c593118f
Elijah Meshnick <emeshnick@gmail.com>
2021-02-15 11:21:57 -0500

    fix: checkboxes in Headings responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
-import {AppBar, Toolbar, IconButton, Typography} from '@material-ui/core';
-import { withStyles} from '@material-ui/core/styles';
-import MailIcon from '@material-ui/icons/Mail';
-import Button from '@material-ui/core/Button';
-import { Link as RouterLink } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import MailIcon from "@material-ui/icons/Mail";
+import Button from "@material-ui/core/Button";
+import { Link as RouterLink } from "react-router-dom";
 
 const navStyles = (theme) => ({
   root: {
@@ -13,16 +11,16 @@ const navStyles = (theme) => ({
   },
   title: {
     flexGrow: 1,
-    fontFamily: 'Potta One cursive',
+    fontFamily: "Potta One cursive",
   },
   homeButton: {
     marginRight: theme.spacing(2),
   },
   toolbarButtons: {
-    float: 'right',
+    float: "right",
   },
   email: {
-    fontSize:'14px'
+    fontSize: "14px",
   },
 });
 
@@ -32,7 +30,7 @@ class NavBar extends Component {
     //   setAnchorEl(event.currentTarget);
     // };
     const classes = this.props.classes;
-    const menuId = 'primary-mail';
+    const menuId = "primary-mail";
     return (
       <div className={classes.root}>
         <AppBar color="primary" position="static">
@@ -42,27 +40,21 @@ class NavBar extends Component {
               edge="start"
               className={classes.homeButton}
               color="inherit"
-              aria-label="home">
-
-            <img alt="uniweb-logo" src="/logo-uniweb-navbar.png"/>
+              aria-label="home"
+            >
+              <img alt="uniweb-logo" src="/logo-uniweb-navbar.png" />
             </IconButton>
 
             <Typography variant="h6" className={classes.title}></Typography>
 
             <div className={classes.toolbarButtons}>
-            <Button
-              component={RouterLink}
-              to={'/about'}
-              color="inherit">
-             About
-             </Button>
+              <Button component={RouterLink} to={"/about"} color="inherit">
+                About
+              </Button>
 
-             <Button
-              component={RouterLink}
-              to={'/contactus'}
-              color="inherit">
-             Contact Us
-             </Button>
+              <Button component={RouterLink} to={"/contactus"} color="inherit">
+                Contact Us
+              </Button>
 
               <IconButton
                 aria-label="send data to email"
@@ -70,10 +62,10 @@ class NavBar extends Component {
                 aria-haspopup="true"
                 //onClick={handleProfileMenuOpen}
                 color="inherit"
-              ><Typography className={classes.email}></Typography>
+              >
+                <Typography className={classes.email}></Typography>
                 <MailIcon />
               </IconButton>
-
             </div>
           </Toolbar>
         </AppBar>
